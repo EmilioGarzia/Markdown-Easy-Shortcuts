f43695e5d6007cd3db4e027f40ad3f504182e4f3
luigimarino01 <luigimarinodev@gmail.com>
2024-04-17 13:51:29 +0200

    default configuration updated for markdown files

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,6 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-shortcuts" is now active!');
